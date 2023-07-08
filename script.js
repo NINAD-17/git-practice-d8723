d5859685eba4af79f6c37d197b7cd773a0eb4364
Ninad <ninadkdhulap17@outlook.com>
2023-07-08 20:03:51 +0530

    some comments are add

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Constants
 const submitBtn = document.getElementById("submitBtn");
 
+
+// Addition function
 function add() {
     const num1 = parseInt(document.getElementById("num1").value);
     const num2 = parseInt(document.getElementById("num2").value);
     console.log(num1 + num2);
 }
 
+// Submit button
 submitBtn.addEventListener("click", add);
